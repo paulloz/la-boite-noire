6f8607c03feea9f73e3d0eadefdd3d140a990006
Paul Joannon <hello@pauljoannon.com>
2018-11-20 12:53:16 +0100

    :lipstick: :iphone:

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 import CircularProgress from '@material-ui/core/CircularProgress';
 
+import Hidden from '@material-ui/core/Hidden';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import Typography from '@material-ui/core/Typography';
 
 import { dsv } from 'd3-fetch';
 
@@ -49,44 +51,69 @@ class DataTable extends Component {
             );
 
         return (
-            <Table className='datatable'>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Données</TableCell>
-                        <TableCell>Branche</TableCell>
-                        <TableCell>Disponibilité</TableCell>
-                        <TableCell>Origine</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
+            <div>
+                <Hidden only={['md', 'lg', 'xl']}>
                     {Object.keys(this.state.data).map((key, i) => {
-                        return this.state.data[key].map((row, j) => {
-                            return (
-                                <TableRow key={i * 1000 + j}>
-                                    {(() => {
-                                        if (j === 0)
-                                            return (
-                                                <TableCell rowSpan={this.state.data[key].length}>{row['donnees']}</TableCell>
-                                            )
-                                        return null;
-                                    })()}
-                                    <TableCell className="nowrap">
-                                        {
-                                            row['lien'] != null
-                                                ? (<a href={row['lien']} target="_blank" rel="noopener noreferrer">{row['branche']}</a>)
-                                                : row['branche']
-                                        }
-                                    </TableCell>
-                                    <TableCell className="nowrap">
-                                        <Dots dispo={row['disponibilite']} granu={row['granularite']} tempo={row['temporalite']} />
-                                    </TableCell>
-                                    <TableCell>{row['origine']}</TableCell>
-                                </TableRow>
-                            );
-                        });
+                        return (
+                            <div className="datatable--mobile">
+                                <Typography variant="subtitle1" className="bold">{key}</Typography>
+                                {this.state.data[key].map((row, j) => {
+                                    return (
+                                        <div>
+                                            {
+                                                row['lien'] != null
+                                                    ? (<a href={row['lien']} target="_blank" rel="noopener noreferrer">{row['branche']}</a>)
+                                                    : row['branche']
+                                            }
+                                            <Dots dispo={row['disponibilite']} granu={row['granularite']} tempo={row['temporalite']} />
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        );
                     })}
-                </TableBody>
-            </Table>
+                </Hidden>
+                <Hidden only={['xs', 'sm']}>
+                    <Table className='datatable'>
+                        <TableHead>
+                        <TableRow>
+                            <TableCell>Données</TableCell>
+                            <TableCell>Branche</TableCell>
+                            <TableCell>Disponibilité</TableCell>
+                            <TableCell>Origine</TableCell>
+                        </TableRow>
+                    </TableHead>
+                        <TableBody>
+                        {Object.keys(this.state.data).map((key, i) => {
+                            return this.state.data[key].map((row, j) => {
+                                return (
+                                    <TableRow key={i * 1000 + j}>
+                                        {(() => {
+                                            if (j === 0)
+                                                return (
+                                                    <TableCell rowSpan={this.state.data[key].length}>{row['donnees']}</TableCell>
+                                                )
+                                            return null;
+                                        })()}
+                                        <TableCell className="nowrap">
+                                            {
+                                                row['lien'] != null
+                                                    ? (<a href={row['lien']} target="_blank" rel="noopener noreferrer">{row['branche']}</a>)
+                                                    : row['branche']
+                                            }
+                                        </TableCell>
+                                        <TableCell className="nowrap">
+                                            <Dots dispo={row['disponibilite']} granu={row['granularite']} tempo={row['temporalite']} />
+                                        </TableCell>
+                                        <TableCell>{row['origine']}</TableCell>
+                                    </TableRow>
+                                );
+                            });
+                        })}
+                    </TableBody>
+                    </Table>
+                </Hidden>
+            </div>
         );
     }
 }
