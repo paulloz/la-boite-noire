dd05dafa85aa10d451d1681d8391eda269f176d1
Paul Joannon <hello@pauljoannon.com>
2019-01-06 16:32:09 +0100

    Create update-data.sh

---
+++ update-data.sh
@@ -0,0 +1,22 @@
+BASEURL="http://lepanierasalade.fr/index/"
+BASEDIR="public/"
+
+UPDATE=""
+
+for FILE in "data.json" "data.tsv"
+do
+    if [ $(md5sum "${BASEDIR}${FILE}" | cut -f1 -d' ') != $(wget -q "${BASEURL}${FILE}" -O- | md5sum | cut -f1 -d' ') ]; then
+        UPDATE="${UPDATE}$(if [ ${#UPDATE} -gt 0 ]; then echo " "; else echo ""; fi)${FILE}"
+        wget -q "${BASEURL}${FILE}" -P ${BASEDIR}
+    fi;
+done
+
+if [ ${#UPDATE} -gt 0 ]; then
+    git fetch origin && git rebase origin/master
+    for FILE in $UPDATE
+    do
+        git add "${BASEDIR}${FILE}"
+    done
+    git commit -m "Update ${UPDATE}"
+    git push origin master:master
+fi;
